48394315413382eb7572465b9faa6b7d84efe758
mrajamanickam <mrajamanickam@salesforce.com>
2017-09-21 11:34:38 -0700

    add gopath, npm to PATH
    add script to remove dup entries in path

--- BASH_IT_CUSTOM/path.bash
+++ BASH_IT_CUSTOM/path.bash
@@ -1,2 +1,18 @@
 # Put brew openssl on top
-export PATH="/usr/local/opt/openssl/bin:$PATH"
+PATH="/usr/local/opt/openssl/bin:$PATH"
+
+# GOPATH
+export GOPATH=$(go env GOPATH)
+PATH="$PATH:$GOPATH/bin"
+
+# npm local bin path
+PATH="./node_modules/.bin:$PATH"
+
+# pipsi - wrapper around pip and virtualenv
+PATH=/Users/mrajamanickam/.local/bin:$PATH
+
+# Remove duplicate entries in PATH
+# https://unix.stackexchange.com/questions/14895/duplicate-entries-in-path-a-problem
+PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
+
+export PATH
